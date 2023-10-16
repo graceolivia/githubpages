d90a01e792b6839e1382f6922bdc73d49be1f9bf
grace <gracelidinskysmith@gmail.com>
2023-10-16 13:44:20 -0400

    Add button to close letter

--- assets/scripts/envelope.js
+++ assets/scripts/envelope.js
@@ -3,9 +3,17 @@ console.log("Is this thing on??");
 const envelope = document.getElementById("envelope");
 const letter = document.getElementById("letter");
 const hiddenDiv = document.getElementById("hiddenDiv");
+const closeLetterButton = document.getElementById("closeLetter");
+
 envelope.addEventListener("click", function() {
     hiddenDiv.classList.remove("hidden");
     envelope.classList.add("hidden");
 });
 
 
+function closeLetterResetEnvelope() {
+    hiddenDiv.classList.add("hidden");
+    envelope.classList.remove("hidden");
+}
+
+closeLetterButton.onclick = closeLetterResetEnvelope;
