893c8d32774e10f25b759e072fb5f0b893241a5d
grace <gracelidinskysmith@gmail.com>
2023-10-16 13:09:44 -0400

    Add click change

--- assets/scripts/envelope.js
+++ assets/scripts/envelope.js
@@ -1,6 +1,11 @@
 console.log("Is this thing on??");
 
+const envelope = document.getElementById("envelope");
 const letter = document.getElementById("letter");
-letter.addEventListener("click", function() {
-    alert("You clicked me!");
+const hiddenDiv = document.getElementById("hiddenDiv");
+envelope.addEventListener("click", function() {
+    hiddenDiv.classList.remove("hidden");
+    envelope.classList.add("hidden");
 });
+
+
